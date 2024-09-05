6acb0a1f110595a7adf2fcbda34c9b193c088c25
Roh1mm <h1101231052@student.untan.ac.id>
2024-09-05 09:01:09 +0700

    Praktikum 4d- Membuat class dari class digaram

---
+++ empat.php
@@ -0,0 +1,97 @@
+<?php
+
+class Author {
+    public $name;
+    public $description;
+
+    public function __construct($name, $description) {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function showtype() {
+        return [
+            'name' => $this->name,
+            'description' => $this->description
+        ];
+    }
+}
+
+class Book {
+    public $ISBN;
+    public $title;
+    public $category;
+    public $description;
+    public $language;
+    public $authorName;
+    public $numberOfPage;
+    public $publisher;
+
+    public function __construct($ISBN, $title, $category, $description, $language, $authorName, $numberOfPage, $publisher) {
+        $this->ISBN = $ISBN;
+        $this->title = $title;
+        $this->category = $category;
+        $this->description = $description;
+        $this->language = $language;
+        $this->authorName = $authorName;
+        $this->numberOfPage = $numberOfPage;
+        $this->publisher = $publisher;
+    }
+
+    public function showAll() {
+        return [
+            'ISBN' => $this->ISBN,
+            'title' => $this->title,
+            'category' => $this->category,
+            'description' => $this->description,
+            'language' => $this->language,
+            'authorName' => $this->authorName,
+            'numberOfPage' => $this->numberOfPage,
+            'publisher' => $this->publisher
+        ];
+    }
+
+    public function detail($ISBN) {
+        if ($this->ISBN == $ISBN) {
+            return $this->showAll();
+        } else {
+            return null;
+        }
+    }
+}
+
+class Publisher {
+    public $name;
+    public $address;
+    private $phone;
+
+    public function __construct($name, $address, $phone) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->phone = $phone;
+    }
+
+    public function setPhone($phone) {
+        $this->phone = $phone;
+    }
+
+    public function getPhone() {
+        return $this->phone;
+    }
+}
+
+// Contoh penggunaan
+$author = new Author("Rohim Amrullah", "An experienced author");
+$book = new Book(123456789, "PHP Programming", "Education", "A comprehensive guide to PHP", "English", "Rohim Amrullah", 300, "Tech Books Publishing");
+$publisher = new Publisher("Tech Books Publishing", "123 Tech Street", "1234567890");
+
+echo "Author: ";
+print_r($author->showtype());
+
+echo "\nBook: ";
+print_r($book->showAll());
+
+echo "\nPublisher Phone: ";
+echo $publisher->getPhone();
+?>
+
