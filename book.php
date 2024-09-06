5a4293e327b52fd5cb5635eeca9135eb093f73a9
Roh1mm <h1101231052@student.untan.ac.id>
2024-09-06 15:03:44 +0700

    Praktikum 5A - Memisahkan Class

---
+++ book.php
@@ -0,0 +1,43 @@
+<?php
+class Book {
+    public $ISBN;
+    public $title;
+    public $category;
+    public $description;
+    public $language;
+    public $authorName;
+    public $numberOfPage;
+    public $publisher;
+
+    public function __construct($ISBN, $title, $category, $description, $language, $authorName, $numberOfPage, $publisher) {
+        $this->ISBN = $ISBN;
+        $this->title = $title;
+        $this->category = $category;
+        $this->description = $description;
+        $this->language = $language;
+        $this->authorName = $authorName;
+        $this->numberOfPage = $numberOfPage;
+        $this->publisher = $publisher;
+    }
+
+    public function showAll() {
+        return [
+            'ISBN' => $this->ISBN,
+            'title' => $this->title,
+            'category' => $this->category,
+            'description' => $this->description,
+            'language' => $this->language,
+            'authorName' => $this->authorName,
+            'numberOfPage' => $this->numberOfPage,
+            'publisher' => $this->publisher
+        ];
+    }
+
+    public function detail($ISBN) {
+        if ($this->ISBN == $ISBN) {
+            return $this->showAll();
+        } else {
+            return null;
+        }
+    }
+}
