85c8ab564fd764056f2b4526f8f5ec62cf0ea72c
Roh1mm <h1101231052@student.untan.ac.id>
2024-09-19 22:57:27 +0700

    Praktikum 6F - Membuat Objek Pewarisan

---
+++ Sebelas.php
@@ -0,0 +1,20 @@
+<?php
+
+// Meng-include file Dosen.php yang berada di folder App/Admin
+require_once 'vendor/autoload.php';
+
+use App\Admin\Dosen;
+
+// Membuat objek baru dari class Dosen
+$dian = new Dosen(
+    1984111320150401001,   // NIP
+    "Dian Prawira",        // Nama
+    62111111,              // No HP
+    "Jln Purnama",         // Alamat
+    "0013118405"           // NIDN
+);
+
+// Menjalankan fungsi mengajar
+$dian->mengajar();
+
+
