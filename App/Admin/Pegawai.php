3b709334cab330c6c19ae753282856623bd4bdd9
Roh1mm <h1101231052@student.untan.ac.id>
2024-09-19 21:38:36 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Admin;
+
+class Pegawai {
+    public int $nip;
+    protected string $nama;
+    protected int $no_hp;
+    protected string $alamat;
+
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat) {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+
+    public function cekIn(): bool {
+        // Implementasi cek in, contoh return true untuk berhasil
+        return true;
+    }
+
+    public function cekOut(): bool {
+        // Implementasi cek out, contoh return true untuk berhasil
+        return true;
+    }
+
+    public function getNoHp(): int {
+        return $this->no_hp;
+    }
+
+    public function setNoHp(int $no_hp): void {
+        $this->no_hp = $no_hp;
+    }
+}
